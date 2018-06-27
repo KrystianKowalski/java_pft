08ccedeae068e2e12f81c414e72d164f269e7ae5
KrystianKowalski <32257076+KrystianKowalski@users.noreply.github.com>
2018-06-27 20:18:47 +0200

    Types if values and variables

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,11 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
    public static void main(String[] args) {
+      String somebody = "world";
+      System.out.println("Hello, " + somebody + "!");
 
-      System.out.println("Hello, world!");
-
-      int l = 2;
-      int s = l*l;
+      double l = 2.0;
+      double s = l*l;
       System.out.println("sqare surface area of " + l + " = " + s);
    }
 }
