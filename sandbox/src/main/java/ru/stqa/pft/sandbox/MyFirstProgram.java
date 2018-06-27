bda7a019ba1170ed2da85e75cbf303f177268d19
KrystianKowalski <32257076+KrystianKowalski@users.noreply.github.com>
2018-06-27 19:54:13 +0200

    Variables and Values

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,5 +5,9 @@ public class MyFirstProgram {
    public static void main(String[] args) {
 
       System.out.println("Hello, world!");
+
+      int l = 2;
+      int s = l*l;
+      System.out.println("sqare surface area of " + l + " = " + s);
    }
 }
